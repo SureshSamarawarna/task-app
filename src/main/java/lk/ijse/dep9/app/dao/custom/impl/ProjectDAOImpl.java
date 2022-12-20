83909186da480d613c3f10f928c2b61d00697622
SureshSamarawarna <suresh.asan8@gmail.com>
2022-12-20 17:14:19 +0530

    create

--- src/main/java/lk/ijse/dep9/app/dao/custom/impl/ProjectDAOImpl.java
+++ src/main/java/lk/ijse/dep9/app/dao/custom/impl/ProjectDAOImpl.java
@@ -1,26 +1,53 @@
 package lk.ijse.dep9.app.dao.custom.impl;
 
+import lk.ijse.dep9.app.dao.custom.ProjectDAO;
 import lk.ijse.dep9.app.dao.custom.UserDAO;
+import lk.ijse.dep9.app.entity.Project;
 import lk.ijse.dep9.app.entity.User;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
-public class ProjectDAOImpl implements UserDAO {
+public class ProjectDAOImpl implements ProjectDAO {
     private Connection connection;
 
     public ProjectDAOImpl(Connection connection) {
         this.connection=connection;
     }
 
+
     @Override
-    public User save(User user) {
-        return null;
+    public Project save(Project project) {
+        try {
+            PreparedStatement stm = connection.
+                    prepareStatement("INSERT INTO Project (username, name) VALUES ( ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, project.getUsername());
+            stm.setString(2, project.getName());
+            stm.executeUpdate();
+            ResultSet generatedKeys = stm.getGeneratedKeys();
+            generatedKeys.next();
+            project.setId(generatedKeys.getInt(1));
+            return project;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void update(User user) {
+    public void update(Project project) {
+
+        try {
+            PreparedStatement stm = connection.prepareStatement("UPDATE Project " +
+                    "SET name = ? AND username = ? WHERE id=?");
+            stm.setString(1,project.getName());
+            stm.setString(2,project.getUsername());
+            stm.setInt(3,project.getId());
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -30,18 +57,39 @@ public class ProjectDAOImpl implements UserDAO {
     }
 
     @Override
-    public Optional<User> findById(String pk) {
+    public Optional<Project> findById(Integer id) {
+        try {
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM Project WHERE id = ?");
+            stm.setInt(1,id);
+            ResultSet rst = stm.executeQuery();
+            if (rst.next()){
+                return Optional.of(new Project(rst.getInt("id"),
+                        rst.getString("name"),
+                        rst.getString("username") ));
+            }
+            return  Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
         return Optional.empty();
     }
 
     @Override
-    public List<User> findAll() {
+    public List<Project> findAll() {
         return null;
     }
 
     @Override
     public long count() {
-        return 0;
+        try {
+            PreparedStatement stm = connection.prepareStatement("SELECT COUNT(id) FROM Project");
+            ResultSet rst = stm.executeQuery();
+            return rst.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
