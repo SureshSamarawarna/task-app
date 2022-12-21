178403ce6dce12275a3df63c7671518e1a3ddc1b
SureshSamarawarna <suresh.asan8@gmail.com>
2022-12-21 13:25:12 +0530

    Refer the jdbc connection pool inside the xml

--- src/main/java/lk/ijse/dep9/app/service/custom/UserServiceImpl.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/UserServiceImpl.java
@@ -1,4 +1,19 @@
 package lk.ijse.dep9.app.service.custom;
 
+import lk.ijse.dep9.app.dao.DAOFactory;
+import lk.ijse.dep9.app.dao.DAOTypes;
+import lk.ijse.dep9.app.dao.custom.UserDAO;
+import lk.ijse.dep9.app.dto.UserDTO;
+
 public class UserServiceImpl implements UserService{
+    private UserDAO userDAO;
+
+    public UserServiceImpl() {
+        userDAO = DAOFactory.getInstance().getDAO(null, DAOTypes.USER,UserDAO.class)
+    }
+
+    @Override
+    public void createNewUserAccount(UserDTO userDTO) {
+
+    }
 }
