44f2fa228b677f147700b294fa10bb0b68bbb27e
SureshSamarawarna <suresh.asan8@gmail.com>
2022-12-22 13:54:46 +0530

    Add ProjectDTO

--- src/main/java/lk/ijse/dep9/app/api/ProjectController.java
+++ src/main/java/lk/ijse/dep9/app/api/ProjectController.java
@@ -1,7 +1,20 @@
 package lk.ijse.dep9.app.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/api/v1/projects")
 public class ProjectController {
+    private void createNewProject(){}
+
+    public void getAllProjects(){}
+
+    public void getProject(){}
+
+    public void renameProject(){}
+
+    public void deleteProject(){}
 }
